6562ff9a59c323f75a3416b0753f6b87be808457
Shafiq Belaroussi <shafiq.belaroussi@gmail.com>
2022-09-11 17:29:18 +0100

    fixed <Head/>

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,25 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				<title>Quran Translations</title>
+				<meta
+					name='description'
+					content='Nottingham Islam: offers multiple Quran transactions'
+				/>
 				<link
 					rel='manifest'
 					href='/manifest.json'
+				/>
+				<link
+					rel='icon'
+					href='/niic.png'
 				/>
 				<link
 					rel='apple-touch-icon'
 					href='/icon.png'></link>
 				<meta
 					name='theme-color'
-					content='#fff'
+					content='#2D8D43'
 				/>
 			</Head>
 			<body>
